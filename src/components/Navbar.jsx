dee9eb4e19227afeec8dd6abca8e317d4d29c9f8
vladdlevshuk <vlad1levshuk@gmail.com>
2024-07-23 01:59:19 +0300

    restyle: improve jumbotron on about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-dark bg-primary navbar-expand-lg px-5 py-3'>
-      <div className="navbar-brand">
-        Github Поиск
+    <nav className='navbar navbar-dark bg-primary navbar-expand px-5 py-3'>
+      <div className="container">
+        <Link to="/" className='navbar-brand'>
+          Github Поиск
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <Link to="/" className='nav-link'>
+                Главная
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className='nav-link'>
+                Информация
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar-nav mx-auto">
-        <li className="nav-item">
-          <Link to="/" className='nav-link'>Главная</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className='nav-link'>Информация</Link>
-        </li>
-      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4">Информация</h1>
-        <p className="lead">Версия приложения: <strong>1.0.0</strong></p>
+    <div className="container py-5">
+      <div className="bg-light p-4 rounded shadow-sm">
+        <h1 className="display-5 text-center">Информация</h1>
+        <p className="lead text-center">Версия приложения: <strong>1.0.0</strong></p>
+        <div className="text-center mt-4">
+          <a href="/" className="btn btn-primary">Вернуться на главную</a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
